e12cc2e3b7a82200176948298f88d5ae54f3c151
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-13 16:09:48 +0530

    minor changes
    What changes you have made?
    1.changed spreading mechanism.
    2.pull out the arePointsEqual function to outside of the class.
    3.changed return statements.
    
    Why this change is necessary?
    ....
    
    What is the impact of the change?
    .....

--- src/line.js
+++ src/line.js
@@ -1,20 +1,25 @@
+const arePointsEqual = (point1, point2) => {
+  const areXPointsEqual = point1.x == point2.x;
+  const areYPointsEqual = point1.y == point2.y;
+  return areXPointsEqual && areYPointsEqual;
+};
+
 class Line {
   constructor(endA, endB) {
-    this.endA = { ...endA };
-    this.endB = { ...endB };
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
-  arePointsEqual(point1, point2) {
-    const areXPointsEqual = point1.x == point2.x;
-    const areYPointsEqual = point1.y == point2.y;
-    return areXPointsEqual && areYPointsEqual;
-  }
+
   isEqualTo(other) {
-    const isSameInstance = other instanceof Line;
-    const areLinesSame =
-      this.arePointsEqual(this.endA, other.endA) &&
-      this.arePointsEqual(this.endB, other.endB);
-    return isSameInstance && areLinesSame;
+    if (other instanceof Line) {
+      return (
+        arePointsEqual(this.endA, other.endA) &&
+        arePointsEqual(this.endB, other.endB)
+      );
+    }
+    return false;
   }
+
   toString() {
     return `Line from (${this.endA.x},${this.endA.y}) to (${this.endB.x},${this.endB.y})`;
   }
