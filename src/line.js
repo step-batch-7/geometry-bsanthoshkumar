573621dd6c01cd2a2f2cc69ac617a51fbcbc0c9f
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-13 11:12:36 +0530

    modified isEqualTo function
    What changes you have made?
    
    1.modified isEqualTo function by checking each point of two lines and
    checking instance of two lines.
    2.modified test cases order and message for isEqualTo function .
    
    Why this change is necessary?
    
    1.to check the equality of values of both lines.
    2.to make the messages meaningful.
    
    What is the impact of the change?
    
    1.we can test whether both lines are same.

--- src/line.js
+++ src/line.js
@@ -1,10 +1,19 @@
 class Line {
   constructor(endA, endB) {
-    this.endA = endA;
-    this.endB = endB;
+    this.endA = { ...endA };
+    this.endB = { ...endB };
+  }
+  arePointsEqual(point1, point2) {
+    const areXPointsEqual = point1.x == point2.x;
+    const areYPointsEqual = point1.y == point2.y;
+    return areXPointsEqual && areYPointsEqual;
   }
   isEqualTo(other) {
-    return this == other;
+    const isSameInstance = other instanceof Line;
+    const areLinesSame =
+      this.arePointsEqual(this.endA, other.endA) &&
+      this.arePointsEqual(this.endB, other.endB);
+    return isSameInstance && areLinesSame;
   }
   toString() {
     return `Line from (${this.endA.x},${this.endA.y}) to (${this.endB.x},${this.endB.y})`;
--- test/testLine.js
+++ test/testLine.js
@@ -3,7 +3,7 @@ const Line = require("../src/line");
 
 describe("Line", function() {
   describe("toString", function() {
-    it("should return empty string", function() {
+    it("should return the string representation of object", function() {
       line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
       const expected = `Line from (1,2) to (3,4)`;
       assert.strictEqual(line1.toString(), expected);
@@ -11,15 +11,16 @@ describe("Line", function() {
   });
 
   describe("isEqualTo", function() {
-    it("should return false for two different objects", function() {
+    it("should return true for two same lines", function() {
       line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
       line2 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
-      assert.strictEqual(line1.isEqualTo(line2), false);
+      assert.strictEqual(line1.isEqualTo(line2), true);
     });
 
-    it("should return true for two different objects", function() {
+    it("should return false for two different lines", function() {
       line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
-      assert.strictEqual(line1.isEqualTo(line1), true);
+      line2 = new Line({ x: 4, y: 3 }, { x: 2, y: 1 });
+      assert.strictEqual(line1.isEqualTo(line2), false);
     });
   });
 });
