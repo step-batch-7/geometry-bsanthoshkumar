2b2ca194bd85f7261e75789e40303110b7d05329
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-17 15:06:07 +0530

    added new property area
    What changes you have made?
    added new property area in Rectangle class
    added test case for area property

--- src/rectangle.js
+++ src/rectangle.js
@@ -8,6 +8,12 @@ class Rectangle {
   toString() {
     return `[Rectangle (${this.diagonal.endA.x},${this.diagonal.endA.y}) to (${this.diagonal.endB.x},${this.diagonal.endB.y})]`;
   }
+
+  get area() {
+    const length = this.diagonal.endB.x - this.diagonal.endA.x;
+    const breadth = this.diagonal.endB.y - this.diagonal.endA.y;
+    return Math.abs(length * breadth);
+  }
 }
 
 module.exports = Rectangle;
--- test/testRectangle.js
+++ test/testRectangle.js
@@ -8,4 +8,11 @@ describe("Rectangle", function() {
       assert.strictEqual(rectangle.toString(), "[Rectangle (1,1) to (5,4)]");
     });
   });
+
+  describe("area", function() {
+    it("should give area of given rectangle", function() {
+      const rectangle = new Rectangle({ x: 1, y: 3 }, { x: 4, y: 1 });
+      assert.strictEqual(rectangle.area, 6);
+    });
+  });
 });
