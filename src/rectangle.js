32acd91007923fa54457917f8155d7c25514fed8
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-17 19:02:20 +0530

    modified constuctor
    What changes you have made?
    modified constructor in Rectangle class by creating points

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,35 +1,37 @@
+const Point = require("./point");
 const Line = require("./line");
 
 class Rectangle {
-  constructor(endA, endB) {
-    this.diagonal = new Line(endA, endB);
+  constructor(vertexA, vertexC) {
+    this.vertexA = new Point(vertexA.x, vertexA.y);
+    this.vertexC = new Point(vertexC.x, vertexC.y);
+    this.vertexB = new Point(vertexC.x, vertexA.y);
+    this.vertexD = new Point(vertexA.x, vertexC.y);
   }
 
   toString() {
-    return `[Rectangle (${this.diagonal.endA.x},${this.diagonal.endA.y}) to (${this.diagonal.endB.x},${this.diagonal.endB.y})]`;
+    return `[Rectangle (${this.vertexA.x},${this.vertexA.y}) to (${this.vertexC.x},${this.vertexC.y})]`;
   }
 
   get area() {
-    const length = this.diagonal.endB.x - this.diagonal.endA.x;
-    const breadth = this.diagonal.endB.y - this.diagonal.endA.y;
-    return Math.abs(length * breadth);
+    const length = this.vertexA.findDistanceTo(this.vertexB);
+    const breadth = this.vertexB.findDistanceTo(this.vertexC);
+    return length * breadth;
   }
 
   get perimeter() {
-    const length = this.diagonal.endB.x - this.diagonal.endA.x;
-    const breadth = this.diagonal.endB.y - this.diagonal.endA.y;
-    return 2 * (Math.abs(length) + Math.abs(breadth));
+    const length = this.vertexA.findDistanceTo(this.vertexB);
+    const breadth = this.vertexB.findDistanceTo(this.vertexC);
+    return 2 * (length + breadth);
   }
 
   isEqualTo(other) {
     if (!(other instanceof Rectangle)) return false;
-    const diagonal2 = new Line(
-      { x: this.diagonal.endB.x, y: this.diagonal.endA.y },
-      { x: this.diagonal.endA.x, y: this.diagonal.endB.y }
-    );
+    const diagonal1 = new Line(this.vertexA, this.vertexC);
+    const diagonal2 = new Line(this.vertexB, this.vertexD);
+    const otherDiagonal = new Line(other.vertexA, other.vertexC);
     return (
-      this.diagonal.isEqualTo(other.diagonal) ||
-      diagonal2.isEqualTo(other.diagonal)
+      diagonal1.isEqualTo(otherDiagonal) || diagonal2.isEqualTo(otherDiagonal)
     );
   }
 }
