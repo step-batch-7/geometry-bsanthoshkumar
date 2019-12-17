b37dc7ad1763d37759f1c00449110ae2b02516c1
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-17 11:44:42 +0530

    added area property
    What changes you have made?
    added area  property circle class
    added test cases to area

--- test/testCircle.js
+++ test/testCircle.js
@@ -9,4 +9,11 @@ describe("Circle", function() {
       assert.strictEqual(circle.toString(), `[Circle @(1,2) radius 5]`);
     });
   });
+
+  describe("area", function() {
+    it("should give area of given circle", function() {
+      const circle = new Circle({ x: 1, y: 2 }, 5);
+      assert.approximately(circle.area, 79, 0.5);
+    });
+  });
 });
